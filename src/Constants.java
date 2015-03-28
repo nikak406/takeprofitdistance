c4c1831651431cbf2463e6a2def183a923789e21
nikak406 <nikak406@gmail.com>
2015-03-28 13:42:42 +0200

    init commit

---
+++ src/Constants.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public interface Constants {
+    double  maxTPD      = 2.1;
+    double  minTPD      = 0.0;
+    double  stepTPD     = 0.1;
+    double maxMult = 1.1;
+    double minMult = 0.0;
+    double stepMult = 0.1;
+    double  goldenratio = 1.618;
+    int     framesizeW   = 580;
+    int     framesizeH   = 300;
+    Font    BIGFONT     = new Font("Sans", Font.BOLD, 20);
+    Font    REGFONT     = new Font("Sans", Font.BOLD, 15);
+    int SPACEW = 9;
+    int SPACEH = 10;
+}
